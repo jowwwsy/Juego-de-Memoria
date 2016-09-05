893d79c9cf7ba675ce0188afc8cccbe6dffe59b5
Joselyne P Banegas <joselynepalmabanegas@macbook-pro-de-joselyne.local>
2016-09-04 23:54:14 -0300

    Initial Commit

---
+++ JuwgodeMemoria/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JuwgodeMemoria
+//
+//  Created by Joselyne Banegas on 04/09/16.
+//  Copyright © 2016 Joselyne Banegas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
