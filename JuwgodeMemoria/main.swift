b33e4089ca3bdf64656e3c54df1d4d59771fe67a
Joselyne P Banegas <joselynepalmabanegas@macbook-pro-de-joselyne.local>
2016-09-05 00:18:24 -0300

    up

--- JuwgodeMemoria/main.swift
+++ JuwgodeMemoria/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+//mudando
+
+// funciona porra!
+
+var numeros = 0...100
+
+//variaveis
+
+for numero in numeros{
+    
+    if  numero >= 30 && numero <= 40 && numero%5 == 0 && numero%2 == 0{
+        print ("\(numero)\t\("PAR, BINGO E VIVA SWIFT")")
+    }
+        
+        // diminui espaco amostral
+        
+    else if numero%2 != 0 && numero >= 30 && numero <= 40 && numero%5 == 0{
+        print ("\(numero)\t\("IMPAR, BINGO E VIVA SWIFT")")
+    }
+        
+        //teste
+        
+    else if numero%2 == 0 && (numero >= 30 && numero <= 40){
+        print ("\(numero)\t\("PAR e VIVA SWIFT")")
+    }
+    else if numero%2 != 0 && (numero >= 30 && numero <= 40){
+        print ("\(numero)\t\("IMPAR e VIVA SWIFT")")
+    }
+    else if numero%2 == 0 && numero%5 == 0{
+        print("\(numero)\t\("PAR E BINGO")")
+    }
+    else  if numero%2 != 0 && numero%5 == 0{
+        print ("\(numero)\t\("IMPAR e BINGO ")")
+    }
+    else if numero%2 == 0{
+        print ("\(numero)\t\("PAR")")
+    }
+    else   if numero%2 != 0{
+        print ("\(numero)\t\("IMPAR")")
+    }
+    
+    
+}
+
 
